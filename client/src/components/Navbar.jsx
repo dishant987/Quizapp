7d815db2c4582c753266aee8b6681bdad76475ff
dishant987 <tankdishant2003@gmail.com>
2024-08-30 14:42:15 +0530

    change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
 
     try {
       const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/logout`, {}, { withCredentials: true });
-      // console.log(response.data)
+      console.log(response.data)
       if (response.status === 200 && response.data.message === "User Logged Out") {
         toast.success(response.data.message);
         removeCookie('quizAccessToken');
